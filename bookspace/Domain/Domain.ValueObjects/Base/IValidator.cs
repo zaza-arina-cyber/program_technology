871a4b7a723add066ebae7cc1e5d5ec568acb137
Ari Enthusiast <Ari Enthusiast@WIN-JMF4ILOB427>
2026-05-29 21:04:37 +0700

    hi

---
+++ bookspace/Domain/Domain.ValueObjects/Base/IValidator.cs
@@ -0,0 +1,14 @@
+﻿namespace bookspace.ValueObjects.Base;
+
+/// <summary>
+/// Defines a method that implements the validation of the object.
+/// </summary>
+/// <typeparam name="T">Type of validation object.</typeparam>
+public interface IValidator<T>
+{
+    /// <summary>
+    /// Validates data.
+    /// </summary>
+    /// <param name="value">The verified value</param>
+    void Validate(T value);
+}
---
+++ bookspace/Domain/Domain.ValueObjects/Exceptions/ValidatorNullException.cs
@@ -0,0 +1,3 @@
+﻿namespace bookspace.ValueObjects.Exceptions;
+public class ValidatorNullException(string paramName)
+: ArgumentNullException(paramName, $"Validator \"{paramName}\" must be specified for type.");
